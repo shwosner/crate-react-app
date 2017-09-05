957242c90229ba2fcdf898954d3548b2694cb696
shwosner <31377730+shwosner@users.noreply.github.com>
2017-09-05 01:01:16 +0100

    added table

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Grid, Jumbotron, Button} from 'react-bootstrap';
 import SearchForm from './components/SearchForm';
 import Results from './components/Results';
+import Table1 from './components/Table';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
           </Grid>
         </Jumbotron>
         <Results/>
+        <Table1/ >
       </div>
     );
   }
